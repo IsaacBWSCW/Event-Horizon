bb96ac22eeed514db5b7ba6eb2305296653b893f
Isla <75605951+potential1energy@users.noreply.github.com>
2025-10-16 13:15:57 +1300

    Add native code

---
+++ native/src/engine.cpp
@@ -0,0 +1,15 @@
+#include <godot_cpp/godot.hpp>
+#include <godot_cpp/classes/node.hpp>
+#include <godot_cpp/core/class_db.hpp>
+
+using namespace godot;
+
+class Engine : public Node
+{
+    GDCLASS(Engine, Node);
+
+public:
+    void say_hello() const {
+        UtilityFunctions::print("Hello from C++");
+    }
+}
